610b9d8ed7ccfaa428a97271e2199536804b6657
Suchismita Sarkar <ssarkar4483@gmail.com>
2026-07-02 02:59:07 +0530

    Add files via upload

---
+++ src/api/capitalGainsData.js
@@ -0,0 +1,13 @@
+// Dummy data exactly as supplied in the assignment's Capital Gains API response.
+export const capitalGainsData = {
+  capitalGains: {
+    stcg: {
+      profits: 70200.88,
+      losses: 1548.53,
+    },
+    ltcg: {
+      profits: 5020,
+      losses: 3050,
+    },
+  },
+};
